89787980c69ff92ce95266e9bc2c0114d05e5a2a
Buryan Nikita <125834786+Inconsp1cuous@users.noreply.github.com>
2023-05-21 08:33:14 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -7,12 +7,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    with open('static/css/test.csv', 'r', encoding='utf-8') as csv_file:
-        csv_data = list(csv.reader(csv_file, delimiter=';'))
+    with open('static/css/jkjk_3.csv', 'r', encoding='utf-8') as csv_file:
+        csv_data = list(csv.reader(csv_file, delimiter=','))
 
     html_table = generate_html_table(csv_data)
 
     return render_template('index.html', html_table=html_table)
+
+    # Прочитайте CSV файл в DataFrame
+    df = pd.read_csv('static/css/jkjk_3.csv', delimiter=',')
+
+    # Конвертируйте DataFrame в формат XLSX
+    xlsx_file = 'static/css/jkjk_3.xlsx'
+    df.to_excel(xlsx_file, index=False)
+
+    return render_template('index.html', xlsx_file=xlsx_file)
 
 
 def generate_html_table(csv_data):
@@ -34,7 +43,7 @@ def generate_html_table(csv_data):
 
 @app.route('/download')
 def download():
-    filename = 'static/css/test.xlsx'
+    filename = 'static/css/jkjk_3.xlsx'
     return send_file(filename, as_attachment=True)
 
 
